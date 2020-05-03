9549c10e347a0761bb703ae54952990bd785056e
bruners101 <brunomallory666@gmail.com>
2020-05-03 17:02:10 +0100

    Add files via upload

---
+++ mergerTimes.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+
+def f(t,r0):
+  m = 1.4 * 2e30
+  G = 6.67e-11
+  c = 3.00e8
+  r = r0**4 - (512 * G**3  * m**3)/(5 * c**5)*t
+  return r
+
+times = []
+seperations = []
+
+r0 = 0
+while True:
+  t = 0
+  seperations.append(r0)
+  while True:
+    r = f(t,r0)
+    if r <= 0:
+      times.append(t)
+      break
+    t+=1
+  r0 += 10**4
+  if r0 > 10**6:
+    break
+
+plt.plot(times,seperations)
+plt.xlabel("Time/s")
+plt.ylabel("Initial Seperation/m")
+plt.show()
